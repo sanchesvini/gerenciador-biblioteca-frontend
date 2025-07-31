ec0b2b151ad31d2a86ef83ee3ee576148c15dc4f
sanchesvini <viniciuus.sanches.ramos@gmail.com>
2025-07-30 22:57:11 -0300

    feat: implements the editarLivro method and adjusts the LivroForm component to create and update books

--- src/components/ItemLivros.ts
+++ src/components/ItemLivros.ts
@@ -2,6 +2,7 @@ import { LivroController } from "../controllers/LivroController";
 
 export async function itemLivros(): Promise<HTMLTableRowElement[]> {
     const livros = await LivroController.listarLivros();
+
     return livros.map(livro => {
         const tr = document.createElement('tr');
         tr.innerHTML = `
@@ -9,11 +10,17 @@ export async function itemLivros(): Promise<HTMLTableRowElement[]> {
             <td>${livro.autor}</td>
             <td>${livro.anoPublicacao}</td>
             <td>
-                <button>Emprestar</button>
-                <button>Editar</button>
-                <button>Excluir</button>
+                <button class="btn-emprestar">Emprestar</button>
+                <button class="btn-editar">Editar</button>
+                <button class="btn-excluir">Excluir</button>
             </td>
         `;
+
+        const botaoEditar = tr.querySelector('.btn-editar');
+        botaoEditar?.addEventListener('click', () => {
+            LivroController.carregarFormEditarLivro(livro.id);
+        });
+
 
         return tr;
     });
